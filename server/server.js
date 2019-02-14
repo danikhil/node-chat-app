adbdb4fd89fb987b60207e8082d73835ba167ddc
danikhil <nikhilkushwaha04@gmail.com>
2019-02-14 10:29:25 +0530

    Doesn't matter

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
-var app = express()
+var app = express();
 
 app.use(express.static(publicPath));
 
